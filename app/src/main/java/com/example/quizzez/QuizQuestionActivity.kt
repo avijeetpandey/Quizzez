204c04f645feb4267ba285d1007ba7d0e8463459
Avijeet pandey <avijeetpandey25@gmail.com>
2024-01-28 00:19:46 +0530

    Add: fetch questions

--- app/src/main/java/com/example/quizzez/QuizQuestionActivity.kt
+++ app/src/main/java/com/example/quizzez/QuizQuestionActivity.kt
@@ -7,5 +7,7 @@ class QuizQuestionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_question)
+
+        val questions = Constants.getQuestions()
     }
 }
