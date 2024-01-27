481818cdc1dbd15b7e93a70082bc0686cacf26ce
Avijeet pandey <avijeetpandey25@gmail.com>
2024-01-28 00:00:13 +0530

    Add: empty name validation

--- app/src/main/java/com/example/quizzez/MainActivity.kt
+++ app/src/main/java/com/example/quizzez/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.quizzez
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.start_button)
+        val edit_text: EditText = findViewById(R.id.name_edit_text)
+        button.setOnClickListener {
+            if(edit_text.text.isEmpty()) {
+                Toast.makeText(this,"Please enter a valid name", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
