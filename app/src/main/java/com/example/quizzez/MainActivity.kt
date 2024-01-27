e24ffab60fe2c31d63f62f74bdcf8633988049fc
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2024-01-28 00:20:24 +0530

    Adds question model (#1)
    * Add: Empty quizez activity
    
    * Add: empty name validation
    
    * Add: Question model and assets
    
    * Add: fetch questions

--- app/src/main/java/com/example/quizzez/MainActivity.kt
+++ app/src/main/java/com/example/quizzez/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.quizzez
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.start_button)
+        val editText: EditText = findViewById(R.id.name_edit_text)
+
+        button.setOnClickListener {
+            if(editText.text.isEmpty()) {
+                Toast.makeText(this,"Please enter a valid name", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this,QuizQuestionActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/quizzez/QuizQuestionActivity.kt
@@ -0,0 +1,13 @@
+package com.example.quizzez
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuizQuestionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_question)
+
+        val questions = Constants.getQuestions()
+    }
+}
