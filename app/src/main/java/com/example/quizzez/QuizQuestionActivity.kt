77471a670968d05d42ae4d1bb006735539d3cb83
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2024-01-28 10:56:13 +0530

    Add: functionality to change option (#3)

--- app/src/main/java/com/example/quizzez/QuizQuestionActivity.kt
+++ app/src/main/java/com/example/quizzez/QuizQuestionActivity.kt
@@ -1,12 +1,21 @@
 package com.example.quizzez
 
+import android.graphics.Color
+import android.graphics.Typeface
+import android.media.tv.TvView
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 
-class QuizQuestionActivity : AppCompatActivity() {
+class QuizQuestionActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var mCurrentPosition: Int = 1
+    private var mQuestionList: ArrayList<Question>? = null
+    private var mSelectedOptionPosition: Int = 0
 
     private var progressBar: ProgressBar? = null
     private var tvProgress: TextView? = null
@@ -22,7 +31,50 @@ class QuizQuestionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_question)
 
-        // binding the view elements
+        mQuestionList = Constants.getQuestions()
+
+        assignElementsFromId()
+
+        setupQuestions()
+
+        tvOptionOne?.setOnClickListener(this)
+        tvOptionTwo?.setOnClickListener(this)
+        tvOptionThree?.setOnClickListener(this)
+        tvOptionFour?.setOnClickListener(this)
+    }
+
+    private fun setupQuestions() {
+        val question = mQuestionList!![mCurrentPosition - 1]
+
+        tvQuestion?.text = question.question
+        progressBar?.progress = mCurrentPosition
+        tvProgress?.text = "${mCurrentPosition} / ${progressBar?.max}"
+
+        ivImage?.setImageResource(question.image)
+
+        tvOptionOne?.text = question.optionOne
+        tvOptionTwo?.text = question.optionTwo
+        tvOptionThree?.text = question.optionThree
+        tvOptionFour?.text = question.optionFour
+    }
+
+    private fun selectedOptionView(tv: TextView, selectedOptionNum: Int) {
+        defaultOptionsView()
+
+        mSelectedOptionPosition = selectedOptionNum
+
+        tv.setTextColor(
+            Color.parseColor("#363A43")
+        )
+
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
+        tv.background = ContextCompat.getDrawable(
+            this@QuizQuestionActivity,
+            R.drawable.selected_option_bg
+        )
+    }
+
+    private fun assignElementsFromId() {
         progressBar = findViewById(R.id.progressBar)
         tvProgress = findViewById(R.id.questionCount)
         tvQuestion = findViewById(R.id.questionTextView)
@@ -32,22 +84,62 @@ class QuizQuestionActivity : AppCompatActivity() {
         tvOptionTwo = findViewById(R.id.optionTwo)
         tvOptionThree = findViewById(R.id.optionThree)
         tvOptionFour = findViewById(R.id.optionFour)
+    }
 
-        val questions = Constants.getQuestions()
+    private fun defaultOptionsView() {
+        val options = ArrayList<TextView>()
 
-        var currentIndex = 3
+        tvOptionOne?.let {
+            options.add(0, it)
+        }
 
-        val question = questions[currentIndex-1]
+        tvOptionTwo?.let {
+            options.add(1, it)
+        }
 
-        tvQuestion?.text = question.question
-        progressBar?.progress = currentIndex
-        tvProgress?.text = "${currentIndex} / ${progressBar?.max}"
+        tvOptionThree?.let {
+            options.add(2, it)
+        }
 
-        ivImage?.setImageResource(question.image)
+        tvOptionFour?.let {
+            options.add(3, it)
+        }
 
-        tvOptionOne?.text = question.optionOne
-        tvOptionTwo?.text = question.optionTwo
-        tvOptionThree?.text = question.optionThree
-        tvOptionFour?.text = question.optionFour
+        for (option in options) {
+            option.setTextColor(Color.parseColor(("#7A8089")))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(
+                this@QuizQuestionActivity,
+                R.drawable.default_option_border_bg
+            )
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.optionOne -> {
+                tvOptionOne?.let {
+                    selectedOptionView(it, 1)
+                }
+            }
+
+            R.id.optionTwo -> {
+                tvOptionOne?.let {
+                    selectedOptionView(it, 2)
+                }
+            }
+
+            R.id.optionThree -> {
+                tvOptionOne?.let {
+                    selectedOptionView(it, 3)
+                }
+            }
+
+            R.id.optionFour -> {
+                tvOptionOne?.let {
+                    selectedOptionView(it, 4)
+                }
+            }
+        }
     }
 }
